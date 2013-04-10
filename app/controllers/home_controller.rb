ab58bcd4daf49431bdefd9867e40572a8a883b2d
Anil kumar <anil.r@imaginea.com>
2013-04-10 14:21:06 +0530

    added browser dropdown in home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+
   def index
-    @users = User.all
+    #@users = User.all
+    @browser_types = ['Firefox', 'Chrome']
+
+    respond_to do |format|
+      format.html # index.html.haml
+    end
   end
+
 end
