688e13aeec3e7e6be0279cc917ad1a627e1c9121
Anil kumar <anil.r@imaginea.com>
2013-04-12 12:13:11 +0530

    Added text area for the code which user wants to add.

--- app/controllers/addoners_controller.rb
+++ app/controllers/addoners_controller.rb
@@ -1,9 +1,15 @@
 class AddonersController < ApplicationController
-  
+
+  BROWSER_LIST = ["firefox", "chrome"]
+
   def create_extension
     @browser_type = params[:browser_type] || ''
-    respond_to do |format|
-      format.html # create_extension.html.haml
+    if @browser_type.blank? || @browser_type && !BROWSER_LIST.include?(@browser_type.downcase)
+      redirect_to root_path
+    else
+      respond_to do |format|
+        format.html # create_extension.html.haml
+      end
     end
   end
 
